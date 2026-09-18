81b027d26e54ee3c4f6eea1cc121c85cc44cdc83
Student <Student@LabVM2049939.ujku40y03opuzmlkqa1241g2fe.jnbx.internal.cloudapp.net>
2026-09-18 20:08:17 +0200

    Asked for the part 1

---
+++ src/main/java/com/mycompany/prog5121poe/PROG5121POE.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog5121poe;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class PROG5121POE {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("========================");
+        System.out.println("QUICKCHAT APP REGISTRATION");
+         System.out.println("========================");
+         
+         //Asking for the first name
+         
+         System.out.println("Enter your first name: ");
+         String firstName = input.nextLine();
+         
+         //Asking for the last name
+        System.out.println("Enter your last name: ");
+         String lastName = input.nextLine();
+         
+         //Asking for the username
+         System.out.println("Enter your username: ");
+         String username = input.nextLine();
+         
+         //Asking for the password
+         System.out.println("Enter your password: ");
+         String password = input.nextLine();
+         //Asking for a cellphone number 
+         System.out.println("Enter your South African cell phone number: ");
+         String cellphonenumber = input.nextLine();
+    
+    //Results for registration
+         {
+        System.out.println("========================");
+         System.out.println("\nREGISTRATION RESULTS");
+         System.out.println("========================");
+         //Results for username
+         
+         System.out.println("First name successfully captured.");
+   
+         System.out.println("Last name successfully captured.");
+         
+         System.out.println("Username successfully captured.");
+         
+         System.out.println("Password successfully captured.");
+         
+         System.out.println("Cell phone number successfully captured.");
+         System.out.println("User successfully registered.");{
+             
+             System.out.println("========================");
+         System.out.println("\nLOGIN");
+         System.out.println("========================");
+         System.out.println("Enter username: musa_");
+         System.out.println("Enter username: kaamil02");
+         System.out.println("Welcome Kaamil02, we have missed you so musch");
+         
+         }
+        
+    }        
+        
+    }
+}
